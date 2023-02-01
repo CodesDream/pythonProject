9ef6ab2d40a24b43d1f89fe4c0175313af1ec469
CodesDream <zlq6612@126.com>
2023-02-01 11:44:19 +0800

    Update sshConn.py

--- sshConn.py
+++ sshConn.py
@@ -1,27 +1,76 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
 import paramiko
+import urllib.request
 import time
-
-username = "admin"
-password = "Ujiabb.2020"
-ip = "192.168.100.2"
+import requests
 
 
-def conn_dev():
+def get_external_ip():
+    try:
+        external_ipaddr = requests.get("http://jsonip.com/").json().get('ip')
+        return external_ipaddr
+    except:
+        return None
+
+
+def conn_dev(hostname: object, port: object, username: object, password: object, external_ip: object) -> object:
     ssh = paramiko.SSHClient()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    ssh.connect(hostname=ip, port=22, username=username, password=password)
-    print('已连接网络设备', ip)
-    command = ssh.invoke_shell()
-    command.send('sys\n')
-    command.send('inter g0/0/24\n')
-    command.send('port link-type access\n port default vlan 20\n ')
-    command.send('dis current-configuration  interface GigabitEthernet 0/0/24\n')
+    ssh.connect(hostname=hostname, port=port, username=username, password=password)
+    print(time.strftime('%Y-%m-%d %H:%M:%S'), '当前公网地址：', external_ip)
+    print(hostname, '已连接网络设备')
 
-    time.sleep(5)
+    if external_ip == "180.169.94.252":
+        command = ssh.invoke_shell()
+        command.send('sys\n')
+        command.send('nat address-group addresspool\n')
+        command.send('undo section 0\n')
+        command.send('section 180.169.94.253 180.169.94.253\n')
+        command.send('return\n quit\n')
+    elif external_ip == "180.169.94.253":
+        command = ssh.invoke_shell()
+        command.send('sys\n')
+        command.send('nat address-group addresspool\n')
+        command.send('undo section 0\n')
+        command.send('section 180.169.94.252 180.169.94.252\n')
+        command.send('return\n quit\n')
+    else:
+        pass
+
+    time.sleep(10)
     output = command.recv(65535)
     print(output.decode().strip())
     ssh.close()
 
 
 if __name__ == '__main__':
-    conn_dev()
+    url = 'https://docker.io/'
+    ip = "192.168.100.1"
+    port = 22
+    username = "admin"
+    password = "g3z&ZKwU4"
+
+    while True:
+        external_ip = get_external_ip()
+
+        opener = urllib.request.build_opener()
+        opener.addheaders = [('User-agent', 'Mozilla/49.0.2')]
+
+        try:
+            opener.open(url)
+            print(url + '此链接访问正常')
+            print(time.strftime('%Y-%m-%d %H:%M:%S'), '当前公网地址：', external_ip)
+            time.sleep(2)
+        except urllib.error.HTTPError:
+            print(url + '此链接不能访问')
+            time.sleep(2)
+            conn_dev(ip, port, username, password, external_ip)
+            print('公网地址已自动更改，当前ip地址为：', external_ip)
+        except urllib.error.URLError:
+            print(url + '此链接不能访问')
+            time.sleep(2)
+            conn_dev(ip, port, username, password, external_ip)
+            print('公网地址已自动更改，当前ip地址为：', external_ip)
+
+        time.sleep(800)
